34be187e5c2d05f53cf485ab04ed913c64d22299
mustafaa <mustafa.asan@comodo.com>
2019-08-02 10:24:26 +0300

    change detection strategy has been updated for navbar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { User } from 'src/app/user/user';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {
   @Input() user: User;
